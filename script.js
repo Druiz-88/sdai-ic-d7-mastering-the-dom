bdee69f4f1b68b3ba89fff45b20a194d15f30a59
Druiz-88 <ngrayven@gmail.com>
2024-11-15 04:30:57 +0000

    added buttons & removal of buttons

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Write your JavaScipt code below this line. 
+const mainTitle = document.getElementById("main-title")
+mainTitle.textContent = "I Really Dislike Cats and Do Not want to do This"
+const description = document.getElementById("description")
+description.textContent = "Cats are the worst"
+
+const cat1button = document.getElementById("cat-1-button")
+cat1button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-1")
+    alert("Goodbye Dumb Cat")
+    cat.remove()
+})
+
+const cat2button = document.getElementById("cat-2-button")
+cat2button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-2")
+    alert("Goodbye Dumb Cat")
+    cat.remove()
+})
+
+const cat3button = document.getElementById("cat-3-button")
+cat3button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-3")
+    alert("Goodbye Dumb Cat")
+    cat.remove()
+})
